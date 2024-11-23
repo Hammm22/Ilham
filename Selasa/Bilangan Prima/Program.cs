43eecd3b9ff2fb5fa01a908307c1b4ff63e19e13
Hammm22 <ilhamdwiarsandy61@gmail.com>
2024-11-23 14:16:25 +0700

    Add files via upload

---
+++ Selasa/Bilangan Prima/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilangan_Prima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka;
+            int i, x;
+            Console.WriteLine("Program sederhana Bilangan Prima");
+            Console.Write("Masukkan Angka: ");
+            angka = int.Parse(Console.ReadLine());
+            if (angka == 1)
+            {
+                Console.WriteLine("Bukan bilangan Prima");
+            }
+            else if (angka == 2)
+            {
+                Console.WriteLine("Bilangan Prima");
+            }
+            else
+            {
+                for (i = 2; i < angka; i++)
+                {
+                    x = angka % i;
+
+                    if (x == 0)
+                    {
+                        Console.WriteLine("Bukan Bilangan Prima");
+                        Console.Read();
+                    }
+                }
+                Console.WriteLine("Bilangan Prima");
+            }
+            Console.ReadKey();
+        }
+    }
+}
